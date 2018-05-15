5b5c8f86eff16e373a3163c954540b294f3e0121
Abdo <abdofoda@stud.fci-cu.edu.eg>
2018-05-15 08:41:31 +0200

    fix search algorithm and _ bug

--- crossword_puzzle.pl
+++ crossword_puzzle.pl
@@ -30,6 +30,22 @@ crossword([H1,H2,H3,H4,V1,V2,V3,V4]):-
     printAnswer([H1,H2,H3,H4],[V1,V2,V3,V4],1).
 
 
+printAnswer(_,_,10).
+printAnswer(H,V,LineNumber):-
+    1 is LineNumber mod 2 ,
+    printIthChar(V,LineNumber),
+    NxtLine is LineNumber+1,
+    printAnswer(H,V,NxtLine).
+
+printAnswer(H,V,LineNumber):-
+    0 is LineNumber mod 2,
+    Hidx is  LineNumber/2,
+    nth1(Hidx,H,Hi),
+    write(Hi),
+    nl,
+    NxtLine is LineNumber+1,
+    printAnswer(H,V,NxtLine).
+
 
 
 substitute(_, [], _, []):-!.
@@ -96,41 +112,34 @@ blank_up([R1,R2,R3,R4,R5,R6,R7,R8,R9],S,Val):-
 go(Start):-
 	path(start,[]).
 
-path([],[]):-
+
+
+
+ play:-
+    crossword(Goal),
+    path( [[[z,o,m,b,i,f,i,e,s],[a,k,e,c,a,b,e,l,e],[b,r,i,c,k,w,o,r,k],[b,a,c,k,c,h,e,c,k],[a,c,m,r,r,e,m,a,d],[n,h,g,w,p,f,a,b,z],[j,e,l,l,y,b,e,a,n], [e,a,r,r,e,o,d,e,d],[a,a,a,a,a,a,a,a,a]]],[],Goal).
+
+%main predicate that takes open list, closed list and goal state
+path([],_,_):-
 		write('No solution'),nl,!.
-path(T, Closed):-
-		
-		crossword(T).
-
-word(zombifies, [z,o,m,b,i,f,i,e,s]).
-word(akecabele, [a,k,e,c,a,b,e,l,e]).
-word(brickwork, [b,r,i,c,k,w,o,r,k]).
-word(backcheck, [b,a,c,k,c,h,e,c,k]).
-word(acmrremad, [a,c,m,r,r,e,m,a,d]).
-word(nhgwpfabz, [n,h,g,w,p,f,a,b,z]).
-word(jellybean, [j,e,l,l,y,b,e,a,n]).
-word(earreoded, [e,a,r,r,e,o,d,e,d]).
-
-word(aaaaaaaaa,[a,a,a,a,a,a,a,a,a]).
+path([[Goal,Parent] | _], Closed, Goal):-
+		write('A solution is found'), nl ,
+		printsolution([Goal,Parent],Closed),!.
+path(Open, Closed, Goal) :-
+		removeFromOpen(Open, [State, Parent], RestOfOpen),
+		getchildren(State, RestOfOpen, Closed, Children),
+		addListToOpen(Children , Open, NewOpen),
+		path(NewOpen, [[State, Parent] | Closed], Goal).
 
 
-play:-
-path( [[[z,o,m,b,i,f,i,e,s],[a,k,e,c,a,b,e,l,e],[b,r,i,c,k,w,o,r,k],[b,a,c,k,c,h,e,c,k],[a,c,m,r,r,e,m,a,d],[n,h,g,w,p,f,a,b,z],[j,e,l,l,y,b,e,a,n], [e,a,r,r,e,o,d,e,d],[a,a,a,a,a,a,a,a,a]]],[]).
-path(Open, Closed):-
-		removeFromOpen(Open, State, RestOfOpen),
-		getchildren(State, State, Open, Closed, Children),
-		write(Children),
-		addListToOpen(Children , RestOfOpen, NewOpen).
 
-getchildren(_, [], _ ,_ , []).
-getchildren(_,_,_,_, []).
-getchildren(Board, [H|State], Open ,Closed , Children):-
-		% moves( Board, H, Open, Closed, X),
-		bagof(X, moves(State, Open, Closed, X), Children), 
-		write(X),
-		write(Children),nl.
-		getchildren(Board, State, Open ,Closed , [Children,_]).
+getchildren(State, Open ,Closed , Children):-
+		bagof(X, moves(State , Open, Closed, X), Children) , !.
+		% write(X),
+		% write(Children),nl.
+		% getchildren(State, Open ,Closed , [Children,_]).
 
+getchildren( _ , _ , _ , []).
 
 addListToOpen(Children, [], Children).
 addListToOpen(Children, [H|Open], [H|NewOpen]):-
@@ -138,7 +147,8 @@ addListToOpen(Children, [H|Open], [H|NewOpen]):-
 
 removeFromOpen([State|RestOpen], State, RestOpen).
 
-moves(State, Val, Open, Closed, Next):-
+moves(State,  Open, Closed, Next) :-
+        member(Val, State),
 		move(State,Next,Val),
 		\+ member([Next,_],Open),
 		\+ member([Next,_],Closed).
@@ -150,7 +160,7 @@ read8words(C):-
     write(' -th word'),nl,
     read(Word),
     stringToChars(Word,Chars),
-    asserta(word(X,Chars)),
+    asserta(word(Word,Chars)),
     C1 is C+1,
     read8words(C1).
 
